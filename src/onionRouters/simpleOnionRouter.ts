c0c48f869210f61cac02f07163e69eae7ab0ee57
elies benmalek <elies.benmalek@edu.devinci.fr>
2024-03-13 08:49:42 +0100

    final

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,21 +1,73 @@
-import bodyParser from "body-parser";
 import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
+import { generateRsaKeyPair, exportPubKey, exportPrvKey, importPrvKey, rsaDecrypt, symDecrypt } from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
-  onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+  var lastReceivedEncryptedMessage: string | null = null;
+  var lastReceivedDecryptedMessage: string | null = null;
+  var lastMessageDestination: number | null = null;
 
+  const { publicKey, privateKey } = await generateRsaKeyPair();
+  const privateKeyBase64 = await exportPrvKey(privateKey);
+  const publicKeyBase64 = await exportPubKey(publicKey);
+
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
+
+  onionRouter.get("/getPrivateKey", (req, res) => {
+    res.json({ result: privateKeyBase64 });
+  });
+
+  const response = await fetch(`http://localhost:${REGISTRY_PORT}/registerNode`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nodeId,
+      pubKey: publicKeyBase64,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to register node: ${response.statusText}`);
+  }
+
+  onionRouter.post("/message", async (req, res) => {
+    const layer = req.body.message; 
+
+    const encryptedSymKey = layer.slice(0, 344);
+    const symKey = privateKeyBase64 ? await rsaDecrypt(encryptedSymKey, await importPrvKey(privateKeyBase64)) : null;
+    const encryptedMessage = layer.slice(344);
+    const message = symKey ? await symDecrypt(symKey, encryptedMessage) : null;
+    lastReceivedEncryptedMessage = layer;
+    lastReceivedDecryptedMessage = message ? message.slice(10) : null;
+    lastMessageDestination = message ? parseInt(message.slice(0, 10), 10) : null;
+    await fetch(`http://localhost:${lastMessageDestination}/message`, {
+      method: "POST",
+      body: JSON.stringify({ message: lastReceivedDecryptedMessage }),
+      headers: { "Content-Type": "application/json" },
+    });
+    res.send("success");
+  });
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
-    console.log(
-      `Onion router ${nodeId} is listening on port ${
-        BASE_ONION_ROUTER_PORT + nodeId
-      }`
-    );
+    console.log(`Onion router ${nodeId} is listening on port ${BASE_ONION_ROUTER_PORT + nodeId}`);
   });
 
   return server;
